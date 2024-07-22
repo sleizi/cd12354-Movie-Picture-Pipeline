fb716f38dc8b92e8d242d5a5f5d64cca61d055c4
tunx7 <cuusoinoi@gmail.com>
2024-07-22 08:51:34 +0700

    first commit

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,5 @@ R
 ".github/workflows/frontend-ci.yaml,b\3\b369b71bf19254afdd73fe5fce091e6fb2b396db
 _
 /starter/frontend/src/components/MovieDetails.js,7\c\7c812e92e8b494749b9550f05bfd52932b5de2d4
+\
+,starter/frontend/src/components/MovieList.js,c\c\cc96f511f5e8a2b205364ae353d8e0650b019590
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,7 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://a5ba0a3ac8afc4fcdb0f1d46d07f3d73-411703249.us-east-1.elb.amazonaws.com/movies`).then((response) => {
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
@@ -25,6 +25,5 @@ function MovieList({ onMovieClick }) {
 MovieList.propTypes = {
   onMovieClick: PropTypes.func.isRequired,
 };
-
 
 export default MovieList;
